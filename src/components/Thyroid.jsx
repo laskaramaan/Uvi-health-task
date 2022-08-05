d9cd4989431360c00726c5b2f53cefd5da72cbac
amaan laskar <laskaramaan@gmail.com>
2022-08-05 21:48:29 +0530

    first component

---
+++ src/components/Thyroid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Thyroid() {
+  return (
+    <div>Thyroid</div>
+  )
+}
+
+export default Thyroid
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Thyroid from '../components/Thyroid'
+
+
+function Home() {
+  return (
+    <>
+    <Thyroid/>
+    
+    </>
+  )
+}
+
+export default Home
