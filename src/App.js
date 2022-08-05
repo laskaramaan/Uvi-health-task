5546d829838fb2b18b86ca2348ed6a21a6c841d2
amaan laskar <laskaramaan@gmail.com>
2022-08-06 01:41:45 +0530

    Created Footer.jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './index.css';
 import Home from '../src/pages/Home'
 import Condition from './components/Condition';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='w-full'>
       <Home/>
       <Condition/>
+      <Footer/>
     </div>
   );
 }
