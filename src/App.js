417b7a7913edb63fab8ecaf4517e992688c6da84
amaan laskar <laskaramaan@gmail.com>
2022-08-06 01:22:40 +0530

    condition upper part done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './index.css';
 import Home from '../src/pages/Home'
+import Condition from './components/Condition';
 
 function App() {
   return (
     <div className='w-full'>
       <Home/>
+      <Condition/>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+
+function Card({ items }) {
+    return (
+        <>
+            <div className="flex md:flex-col md:items-center gap-4 w-full md:w-auto md:h-[230px] bg-white  rounded-xl ">
+                {/* Condition upper part */}
+                <div
+                    className=" h-[140px] w-[230px] md:w-[120px] xl:w-[160px] py-2 font-semibold p-2 bg-[#373750] text-white text-lg leading-5
+                 flex flex-col items-center gap-2 rounded-xl"
+                >
+                    <img src={items.img} className="w-[70px]" />
+                    <p className="text-center">{items.title}</p>
+                </div>
+                
+
+            </div>
+        </>
+    )
+}
+
+export default Card
