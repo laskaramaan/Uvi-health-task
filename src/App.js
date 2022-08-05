6052d7b98607dfd7989c6f03f40e86333ccde437
amaan laskar <laskaramaan@gmail.com>
2022-08-05 21:44:13 +0530

    imported all the assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import './index.css';
-
+import Home from '../src/pages/Home'
 
 function App() {
   return (
     <div className='w-full'>
-      
+      <Home/>
     </div>
   );
 }
