650a69281972098b4085043972a638c31b33e16d
amaan laskar <laskaramaan@gmail.com>
2022-08-06 01:02:48 +0530

    imported assets in Conditon.js

---
+++ src/components/Condition.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Inflammation from "../assets/pic/Inflammation.svg";
+import Stomach from "../assets/pic/stomach.svg";
+import Metabolism from "../assets/pic/metabolism.svg";
+import Medicine from "../assets/pic/medicine.svg";
+import Genetics from "../assets/pic/genetics.svg";
+
+function Condition() {
+  return (
+    <div>Condition</div>
+  )
+}
+
+export default Condition
