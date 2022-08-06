72da90d2f31c9d5b2e260f26dda45c763d4acccf
amaan laskar <laskaramaan@gmail.com>
2022-08-06 11:03:04 +0530

    imported all assets in Explained.jsx

--- src/components/Explained.jsx
+++ src/components/Explained.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
+import Batterylow from '../assets/pic/Batterylow.svg'
+import check from '../assets/pic/check.svg'
+import cancel from '../assets/pic/x-circle.svg'
+import food from '../assets/pic/food.svg'
+import Avoidweb from '../assets/pic/Avoidweb.svg'
+import avoid from '../assets/pic/avoid.svg'
+import minus from '../assets/pic/minus.svg'
+import alcohol from '../assets/pic/alcohol.svg'
+import Stress from '../assets/pic/Stress.svg'
+import yoga from '../assets/pic/yoga.svg'
+import breathing from '../assets/pic/breathing.svg'
+import sleep from '../assets/pic/sleep.svg'
+import vimanin from '../assets/pic/Vitamin.svg'
+import plus from '../assets/pic/plus.svg'
+import arrow from '../assets/pic/arrow.svg'
+import Environment from '../assets/pic/Environment.svg'
+import limit from '../assets/pic/limit.svg'
+
 function Explained() {
   return (
     <div>Explained</div>
