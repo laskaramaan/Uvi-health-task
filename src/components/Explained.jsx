2f9d52ce022651cc23baf0ffe178aa07c49c4979
amaan laskar <laskaramaan@gmail.com>
2022-08-06 19:42:46 +0530

    Completed the factors component

--- src/components/Explained.jsx
+++ src/components/Explained.jsx
@@ -12,7 +12,7 @@ import Stress from '../assets/pic/Stress.svg'
 import yoga from '../assets/pic/yoga.svg'
 import breathing from '../assets/pic/breathing.svg'
 import sleep from '../assets/pic/sleep.svg'
-import vimanin from '../assets/pic/Vitamin.svg'
+import Vitamin from "../assets/pic/Vitamin.svg"
 import plus from '../assets/pic/plus.svg'
 import arrow from '../assets/pic/arrow.svg'
 import Environment from '../assets/pic/Environment.svg'
@@ -34,6 +34,66 @@ function Explained() {
         para2:"Practice elimination diet under guidance",
         img3:food,
         para3:"Eat nutrient dense foods"
+
+    },{
+        img:Avoidweb,
+        value:"33",
+        strokes:"rgba(143, 188, 143, 33)",
+        trails:"rgba(143, 188, 143, 0.4)",
+        title:"Mild",
+        heading:"Gut Health",
+        paragraph:"Irritable bowel, diarrhoea, food intolerance or constipation are the biggest signs of teaky gut, i.e. presence of microbes in the digestive tract. Gut health and thyroid are closely inter-connected as your gut houses the most immune cells in body.",
+        img1:avoid,
+        para1:"Avoid Dairy Products",
+        img2:minus,
+        para2:"Cut Processed/ high sugar foods",
+        img3:alcohol,
+        para3:"Eat nutrient dense foods"
+
+    },{
+        img:Stress,
+        value:"90",
+        strokes:"rgba(255, 0, 0, 90)",
+        trails:"rgba(255, 0, 0, 0.4)",
+        title:"Severe",
+        heading:"Metabolic Stress",
+        paragraph:"Irritable bowel, diarrhoea, food intolerance or constipation are the biggest signs of teaky gut, i.e. presence of microbes in the digestive tract. Gut health and thyroid are closely inter-connected as your gut houses the most immune cells in body.",
+        img1:breathing,
+        para1:"Practice deep breathing/ meditation",
+        img2:yoga,
+        para2:"Do yoga & strength exercises",
+        img3:sleep,
+        para3:"Sleep 7-8 hours to keep stress in check"
+
+    },{
+        img:Vitamin,
+        value:"57",
+        strokes:"rgba(255, 255, 0, 65)",
+        trails:"rgba(255, 255, 0, 0.4)",
+        title:"Moderate",
+        heading:"Nutritional Deficiencies",
+        paragraph:"common nutritional deficiencies of vitamin D, vitamin B12, vitamin A, vitamin B2 Vitamin C, Magnesium or Zinc can contribute to severity of hypothyroidism.",
+        img1:check,
+        para1:"Add supplements to your diets",
+        img2:plus,
+        para2:"Include magnesium rich food",
+        img3:arrow,
+        para3:"Improve dietary fibre intake"
+
+    },{
+        img:Environment,
+        value:"58",
+        strokes:"rgba(255, 255, 0, 65)",
+        trails:"rgba(255, 255, 0, 0.4)",
+        title:"Moderate",
+        heading:"Genetics & Enviromnent",
+        paragraph:"Genetic factors play a role in more than 75% of the thyroid cases. Moreover, exposure to toxic like mercury flouride and fertilizer have been found to affect thyroid function by recruiting antibodies to attack the thyroid.",
+        img1:limit,
+        para1:"Limit Toxin Exposure",
+        img2:minus,
+        para2:"Cut down plastic use",
+        img3:sleep,
+        para3:"Sleep 7-8 hours to keep stress in check"
 
     }]
   return (
--- src/components/Factors.jsx
+++ src/components/Factors.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import Consult from "../assets/pic/Consultweb.svg";
+import Book from "../assets/pic/Bookweb.svg";
+import Thyroidweb from "../assets/pic/Thyroidweb.png";
 
 function Factors() {
-  return (
-    <div>Factors</div>
-  )
+    return (
+        <div className='bg-white py-[50px] w-full'>
+            <div className='flex w-11/12 mx-auto flex-col-reverse md:flex-row justify-between gap-2'>
+                <div className='flex flex-col md:w-[45%] justify-center text-[#373750] gap-4 '>
+                    <h2 className='text-xl md:text-2xl xl:text-4xl font-bold pb-3'>
+                        95%+ Hypothyroidism is caused by one of the 3 underlyig factors
+                    </h2>
+                    <p className='text-base md:text-xl font-semibold '>
+                        1. Hahiimoto's
+                    </p>
+                    <p className='text-base md:text-xl font-semibold'>
+                        2. HPA Axis Dysfunction
+                    </p>
+                    <p className='text-base md:text-xl font-semibold'>
+                        3. Estrogen Excess
+                    </p>
+                    <p className='py-3 text-xs md:text-sm font-semibold'>
+                        These root causes are treatable. Book a thyroid root cause panel to find your clinical root cause and begin personalised treatment.
+                    </p>
+                    <div className='flex flex-col items-center md:items-start xl:flex-row gap-4'>
+                        <img className='w-[280px] h-[40px] md:h-[65px]' alt='' src={Consult} />
+                        <img className='w-[280px] h-[40px] md:h-[65px]' alt='' src={Book} />
+                    </div>
+                </div>
+                <div className='md:w-[50%] flex justify-end items-center'>
+                    <img className='w-full md:w-[400px] xl:w-[490px] h-[250px] md:h-[500px]' alt='' src={Thyroidweb} />
+
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Factors
